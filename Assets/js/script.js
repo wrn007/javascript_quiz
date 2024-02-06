4421bdf680215c985bab6431283d3613ce45efd6
Wesley Noble <wrn007@hotmail.com>
2024-02-06 05:13:38 -0500

    fixed highscore button and changed css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,180 +1,162 @@
-let startbutton = document.querySelector("#startButton")
-let submitbutton = document.querySelector("#submitButton")
-let quiz = document.querySelector("#questions")
-let timeEl = document.querySelector("#timer")
-let initials = document.querySelector("#name")
-let scorebutton =document.querySelector("#viewScores")
-let secondsLeft = 60
-let questionGroup = 0
+let startbutton = document.querySelector("#startButton");
+let submitbutton = document.querySelector("#submitButton");
+let quiz = document.querySelector("#questions");
+let timeEl = document.querySelector("#timer");
+let initials = document.querySelector("#name");
+let scorebutton = document.querySelector("#viewScores");
+let secondsLeft = 60;
+let questionGroup = 0;
 let timer;
-let startpage = document.querySelector("#startpage")
-let endpage = document.querySelector("#endPage")
+let startpage = document.querySelector("#startpage");
+let endpage = document.querySelector("#endPage");
 let qtext = document.querySelector('#questext');
-let choices = document.querySelector('#choices')
-let scoreset = []
-let questions = [ 
-	{ 
-        prompt: `which file type is used primarily for customizing the appearance of specific website elements`, 
-		options: [ 
-			".css", 
-			".js",
-			".html", 
-			".pdf", 
-		], 
-		answer: ".css", 
-	}, { 
-        prompt: `what is the standard layout order for these HTML elements?`, 
-		options: [ 
-			"head, header, body ", 
-			"header, head, body", 
-			"head, body, header", 
-			"body, head, header", 
-		], 
-		answer: "head, body, header", 
-	}, { 
-		prompt: `what is the proper way to reference something with the ID of element`, 
-		options: [ 
-			"#element", 
-			".element", 
-			"!element", 
-			"?element", 
-		], 
-		answer: "#element", 
-	}, { 
-		prompt: `what is the proper comparator to check that the value and type are equal`, 
-		options: [
-            "=", 
-            "==", 
-            "===", 
-            "!==="
-        ], 
-		answer: "===", 
-	}, { 
-		prompt: `how often will a function call when set to an interval of 1000`, 
-		options: [ 
-			"every 1 second", 
-			"every 1 milisecond", 
-			"every 1000 seconds", 
-			"every 1 minute", 
-		], 
-		answer: "every 1 second", 
-	}, 
-]; 
+let choices = document.querySelector('#choices');
+let scoreset = [];
+let questions = [
+    {
+        prompt: `Which file type is used primarily for customizing the appearance of specific website elements?`,
+        options: [
+            ".css",
+            ".js",
+            ".html",
+            ".pdf",
+        ],
+        answer: ".css",
+    }, {
+        prompt: `What is the standard layout order for these HTML elements?`,
+        options: [
+            "head, header, body ",
+            "header, head, body",
+            "head, body, header",
+            "body, head, header",
+        ],
+        answer: "head, body, header",
+    }, {
+        prompt: `What is the proper way to reference something with the ID of element?`,
+        options: [
+            "#element",
+            ".element",
+            "!element",
+            "?element",
+        ],
+        answer: "#element",
+    }, {
+        prompt: `What is the proper comparator to check that the value and type are equal?`,
+        options: [
+            "=",
+            "==",
+            "===",
+            "!==",
+        ],
+        answer: "===",
+    }, {
+        prompt: `How often will a function call when set to an interval of 1000 milliseconds?`,
+        options: [
+            "Every 1 second",
+            "Every 1 millisecond",
+            "Every 1000 seconds",
+            "Every 1 minute",
+        ],
+        answer: "Every 1 second",
+    },
+];
 
-//when the start button is pressed, this function will run
 function startquiz() {
-    //display questions function should show questions, with options as buttons
-    //when the timer reaches 0 the interval will clear so the timer no longer counts down, and it will run the endQuiz function
-    //the starting page is no longer displayed and the hidden attribute from the quiz element is removed 
-    displayQuestions;
+    displayQuestions();
     timeEl.textContent = secondsLeft;
     timer = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-        if (secondsLeft === 0)
-        {
+        if (secondsLeft === 0) {
             clearInterval(timer);
-            endQuiz;
+            endQuiz();
         }
-        //repeats every 1 sec
-    }, 1000); 
+    }, 1000);
     startpage.setAttribute("class", "hidden");
     quiz.removeAttribute("class");
-   
 }
 
-
-//this function is called from the startquiz function
-//this sets an active question from the questions available in my let statements
-function displayQuestions(){
+function displayQuestions() {
     let questionactive = questions[questionGroup];
-   //sets text element to be the promt value of the active question 
     qtext.textContent = questionactive.prompt;
-    
+
     choices.innerHTML = "";
-    //repeatedly executes
-    questionactive.options.forEach( function (choice, i){
-//creates a button element for each option in the question,
-//gives it a 1 2 3 or 4 depending on which one it is, and appends it to the parent div
-// on click, it will run the choose answer function
-    let answerbutton = document.createElement("button");
-    answerbutton.setAttribute("value", choice);
-    answerbutton.textContent = i + 1 + choice;
-    answerbutton.onclick = chooseanswer;
-    choices.appendChild(answerbutton);
-});}
+    questionactive.options.forEach(function (choice, i) {
+        let answerbutton = document.createElement("button");
+        answerbutton.setAttribute("value", choice);
+        answerbutton.textContent = choice;
+        answerbutton.onclick = chooseanswer;
+        choices.appendChild(answerbutton);
+    });
+}
 
-
-//this function is called when clicking on an option from display questions
-function chooseanswer(){
-    //if the value from choose answer is NOT equal to the answer declared in the let statements,
-    //then it moves onto the next question and adds 10 seconds
-    if (this.value !== questions[questionactive].answer)
-    {
-        questionGroup++; 
+function chooseanswer() {
+    if (this.value !== questions[questionGroup].answer) {
+        questionGroup++;
         secondsLeft -= 10;
-        //double checks to see if the timer is less than 0 after decreasing by 10, if so, update the timer to 0 and
-        //call the end quiz function
-        if (time < 0){
-            time = 0;
-            timer.textContent = secondsLeft;
-            quizEnd();
+        if (secondsLeft < 0) {
+            secondsLeft = 0;
+            timeEl.textContent = secondsLeft;
+            endQuiz();
         }
-        //ends the quiz if the last question is answered
-        if (activequestion === questions.length) { 
-            quizEnd(); 
-        }  
-        //display next question
-        else {
-        displayQuestions();
-        } 
-    }
-//if it was the correct answer then it moves onto the next question or ends the quiz if it was the last question
-    if ( this.value === questions[questionGroup].answer)
-    {
-        activequestion++; 
-        if (activequestion === questions.length) { 
-            quizEnd(); 
+        if (questionGroup === questions.length) {
+            endQuiz();
         } else {
             displayQuestions();
-        } 
+        }
+    }
+    if (this.value === questions[questionGroup].answer) {
+        questionGroup++;
+        if (questionGroup === questions.length) {
+            endQuiz();
+        } else {
+            displayQuestions();
+        }
     }
 }
-//called when the game ends either by finishing all the questions or the timer running out
+
 function endQuiz() {
-    //recheck the interval was cleared
-    clearInterval(timer)
-    //remove the hidden class from the endpage
- endpage.removeAttribute("class");
- //rehide the quiz section
- quiz.setAttribute("class", "hidden");
- //set your final score to the seconds you had left when it ended
- let finalscore = secondsLeft;
-
+    clearInterval(timer);
+    endpage.removeAttribute("class");
+    quiz.setAttribute("class", "hidden");
+    let finalscore = secondsLeft;
+    document.getElementById("score").textContent = finalscore;
 }
 
-//is called when the user clickes on the submit button
-function submitScore(){
-    let nameinput = initials.value();
-if (nameinput !== null) {
-    //past here i could not add much more. since my earlier code does not work, i could not test or debug anything
-    let storedscores = JSON.parse(localstorage.getItem("highscores"))
-    if (storedscores !== null){
-        scoreset=storedscores;
+function submitScore() {
+    let nameinput = initials.value;
+    if (nameinput !== "") {
+        let storedscores = JSON.parse(localStorage.getItem("highscores")) || [];
+        storedscores.push({ name: nameinput, score: secondsLeft });
+        localStorage.setItem("highscores", JSON.stringify(storedscores));
+        alert("Score submitted! Check to see how you stand amongst your friends.");
     }
-    localStorage.setItem("highscores", JSON.stringify(scoreset));
-    //alert the user that their score has been submitted, and to check their score on the scoreboard
-    alert("score submitted! check to see how you stand amongst your friends.")
-}
 }
 
-function highscores(){
-    
+function highscores() {
+    let storedscores = JSON.parse(localStorage.getItem("highscores")) || [];
+    storedscores.sort((a, b) => b.score - a.score); 
+    let scoresList = document.getElementById("Scores");
+    scoresList.innerHTML = "";
+    storedscores.forEach(score => {
+        let li = document.createElement("li");
+        li.textContent = `${score.name}: ${score.score}`;
+        scoresList.appendChild(li);
+    });
+    startpage.setAttribute("class", "hidden");
+    quiz.setAttribute("class", "hidden");
+    endpage.setAttribute("class", "hidden");
+    let scorechart = document.getElementById("scorechart");
+    scorechart.removeAttribute("class");
+
+    let backButton = document.getElementById("back");
+    backButton.addEventListener("click", function() {
+        scorechart.setAttribute("class", "hidden");
+        startpage.removeAttribute("class");
+    });
 }
+
 
 startbutton.addEventListener("click", startquiz);
-
 scorebutton.addEventListener("click", highscores);
-
 submitbutton.addEventListener("click", submitScore);
-
